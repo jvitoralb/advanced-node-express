c055b3c795668f3d97411be9bc8513a9e621d30e
jvitoralb <jvvalb@gmail.com>
2022-10-14 22:41:32 -0300

    update

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+app.set('view engine', 'pug');
 
+app.route('/').get((req, res) => {
+  res.render(`${process.cwd()}/views/pug/index`, { title:'Hello', message: 'Please login' });
 });
 
 const PORT = process.env.PORT || 3000;
